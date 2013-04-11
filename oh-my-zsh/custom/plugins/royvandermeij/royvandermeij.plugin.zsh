a3fb92a6206df82043d63319678ff5338bd3d430
Roy van der Meij <roy@royapps.nl>
2013-04-11 10:56:19 +0200

    oh-my-zsh royvandermeij plugin

---
+++ oh-my-zsh/custom/plugins/royvandermeij/royvandermeij.plugin.zsh
@@ -0,0 +1,17 @@
+# a few aliases I like
+alias ga='git add'
+alias glog='git log --pretty="format:%C(yellow)%h%Cblue%d%Creset %s %C(white) %an, %ar%Creset"'
+alias gl='glog --graph --all'
+alias gs='git status -sb'
+alias go='git checkout'
+alias gm='git merge --no-ff'
+alias gmf='git merge --ff-only'
+alias gp='gp'
+
+export EDITOR="/usr/bin/mvim"
+alias tree="find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'"
+
+alias gwc='git whatchanged -p --abbrev-commit --pretty=medium'
+
+# add plugin's bin directory to path
+export PATH="$(dirname $0)/bin:$PATH"
